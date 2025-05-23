a2977905cebbe07995e11be4cd47a3441196f61b
AIshaban <adelishaban99@gmail.com>
2025-05-23 03:12:37 +0300

    adding basic style to website
    in this commit i added basic styling using bootstrap to [edit, new, shoe] pages

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 +10);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.floor(Math.random() * 400)}`,
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo architecto commodi explicabo libero. Assumenda dolore doloremque amet sapiente. Dolore, provident quis reprehenderit ex sit impedit molestiae. Ea mollitia eligendi fuga!",
+            price
         })
         await camp.save();
     }
@@ -30,3 +34,4 @@ const seedDB = async () => {
 seedDB().then(() => {
     mongoose.connection.close();
 })
+
