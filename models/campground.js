3044665e5cfaa8fc1b2a1c8c50da82b86d19d068
AIshaban <adelishaban99@gmail.com>
2025-07-19 20:54:21 +0300

    adding Authorization
    adding Authorization  for reviews and users

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Review = require('./review');
+const { ref } = require('joi');
 
 const Schema= mongoose.Schema;
 
@@ -9,6 +10,10 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
     reviews:[
         {
             type: Schema.Types.ObjectId,
