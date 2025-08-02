4cb9a279e2d1779c37c90dbfcbde8e34e37380cc
AIshaban <adelishaban99@gmail.com>
2025-08-02 23:30:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ const store = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
   crypto: {
-    secret: "thisshouldbeabettersecret!",
+    secret: process.env.SECRET,
   },
 });
 
@@ -108,7 +108,7 @@ store.on("error", function (e) {
 const sessionConfig = {
   store,
   name: "session",
-  secret: "thisshouldbeabettersecret!",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
